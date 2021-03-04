0dcc8e871699a04ce6208143616cb0308646d76b
shlomi d <shlomi1126@gmail.com>
2021-03-04 13:57:42 +0200

    first commit

---
+++ src/main/java/ibm/TestOpenSHiftApplication.java
@@ -0,0 +1,13 @@
+package ibm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOpenSHiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOpenSHiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibm/TestOpenSHiftApplicationTests.java
@@ -0,0 +1,13 @@
+package ibm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOpenSHiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
